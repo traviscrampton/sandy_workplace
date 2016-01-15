7ada0d3e84f3751dea0afed11ebbc08159bed9b9
Travis Crampton <crampton.travis@gmail.com>
2016-01-15 11:36:11 -0800

    configure heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,19 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
 
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'localhost:3000',
+      user_name:            'huasnawood@gmail.com',
+      password:             'entersandman',
+      authentication:       'plain',
+      enable_starttls_auto: true,
+       openssl_verify_mode:   'none'
+    }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
