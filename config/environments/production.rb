2e1b1e0ff19633dde549a81b76651fc14cbf95a4
Travis Crampton <crampton.travis@gmail.com>
2016-03-03 15:04:05 -0800

    changed production stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3-region => "US Standard"
-  :s3-credentials => {
+  :s3_region => "US Standard"
+  :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
