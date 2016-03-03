cc1999eb90815d4c8ebe526fa7ecf7e0dabd7556
Travis Crampton <crampton.travis@gmail.com>
2016-03-03 12:21:23 -0800

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  config.action_mailer.default_url_options = { :host => "gmail.com" }
+  config.action_mailer.default_url_options = { :host => 'huasnawood.com' }
 
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
