8755d8ad76f433a1aa10e430390bfd5e02be2526
Travis Crampton <crampton.travis@gmail.com>
2016-03-03 11:51:09 -0800

    fixing the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'localhost:3000',
-      :user_name =>            'huasnawood@gmail.com',
+      user_name:         'travisappdomain@gmail.com',
       password:             'entersandman',
       authentication:       'plain',
       enable_starttls_auto: true,
